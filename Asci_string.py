ef28bd2aaf6a639ee92c13448928b70c0eba151b
vaishzzz <vaishnaviganeshan16@gmail.com>
2025-11-02 11:51:57 +0530

    Initial commit

--- Asci_string.py
+++ Asci_string.py
@@ -1,3 +1,4 @@
+# This program performs ROT13 encryption/decryption on a given string.
 lists = {'a':'n','b':'o','c':'p','d':'q','e':'r','f':'s','g':'t','h':'u','i':'v','j':'w','k':'x','l':'y','m':'z','n':'a','o':'b','p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j','x':'k','y':'l','z':'m','A':'N','B':'O','C':'P','D':'Q','E':'R','F':'S','G':'T','H':'U','I':'V','J':'W','K':'X','L':'Y','M':'Z','N':'A','O':'B','P':'C','Q':'D','R':'E','S':'F','T':'G','U':'H','V':'I','W':'J','X':'K','Y':'L','Z':'M'}
 def ASCI_string(string):
     result = ""
@@ -12,5 +13,6 @@ def main():
     input_string=input("enter")
     output_string=ASCI_string(input_string)
     print("the converted string is:",output_string)
+
 if __name__ == "__main__":
     main()
